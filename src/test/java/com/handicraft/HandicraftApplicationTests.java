ef88565fd3f0212974357aed3a340a7582c0ad03
Jjenny-K <kkjjh0129@gmail.com>
2024-01-24 18:44:02 +0900

    Add: Initial settings

---
+++ src/test/java/com/handicraft/HandicraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.handicraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandicraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
