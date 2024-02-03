ede7bfa2479e50bd9418ab6f40b7c43c0fbe82ed
Jjenny-K <kkjjh0129@gmail.com>
2024-02-03 21:06:01 +0900

    Feat: #3 Initial follow entity

---
+++ src/main/java/com/preorderpurchase/domain/entity/Follow.java
@@ -0,0 +1,30 @@
+package com.preorderpurchase.domain.entity;
+
+import com.preorderpurchase.domain.core.BaseCreated;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity(name = "follows")
+@Table(name = "follows")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Follow extends BaseCreated {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "followId")
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "followerUserId")
+    private CustomUser followerUser;
+
+    @ManyToOne
+    @JoinColumn(name = "followingUserId")
+    private CustomUser followingUser;
+
+}
