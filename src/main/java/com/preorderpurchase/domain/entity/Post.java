2d99e046a8352a80b78cc59d07d539b49e5bfb5d
Jjenny-K <kkjjh0129@gmail.com>
2024-02-06 11:28:20 +0900

    Feat: #4 Initial post entity

---
+++ src/main/java/com/preorderpurchase/domain/entity/Post.java
@@ -0,0 +1,32 @@
+package com.preorderpurchase.domain.entity;
+
+import com.preorderpurchase.domain.core.BaseCreatedUpdated;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity(name = "posts")
+@Table(name = "posts")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Post extends BaseCreatedUpdated {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "postId")
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "postUser")
+    private CustomUser postUser;
+
+    @Column(name = "title", nullable = false, length = 100)
+    private String title;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+}
